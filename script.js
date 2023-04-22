16be8de27c8fcd6ed570624ba977a22260a953c9
Ilya Kotovskiy <ilundrik026@gmail.com>
2023-04-22 22:17:55 +0300

    fix 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const items = [];
+const table = document.getElementById('table');
 
 // Функция чтения данных из файла
 function loadCSV() {
@@ -6,74 +6,34 @@ function loadCSV() {
         .then(response => response.text())
         .then(data => {
             const items = data.trim().split('\n').map(line => {
-                const values = line.split(';');
-                return {
-                    from: values[0],
-                    towhere: values[1],
-                    date: values[2],
-                    type: values[3]
-                };
+                const [from, towhere, date, type] = line.split(';');
+                return { from, towhere, date, type };
             });
             return items;
         });
 }
 
 // Функция вывода одного объекта из массива
-function getTableItem(itemObj) {
-    const container = document.createElement('div');
-    const from = document.createElement('div');
-    const towhere = document.createElement('div');
-    const date = document.createElement('div');
-    const type = document.createElement('div');
-
-    container.classList.add('row');
-    from.classList.add('cell')
-    towhere.classList.add('cell')
-    date.classList.add('cell')
-    type.classList.add('cell')
-
-    from.textContent = itemObj.from
-    towhere.textContent = itemObj.towhere
-    date.textContent = itemObj.date
-    type.textContent = itemObj.type
-
-    container.append(from, towhere, date, type);
-
-    return container
-}
-
-
-const table = document.getElementById('table');
-
-fetch('Skolopendra_Kei_774_s_1_dopolnenie_1__1.csv')
-    .then(response => response.text())
-    .then(data => {
-        const items = [];
-        data.trim().split('\n').forEach(line => {
-            const values = line.split(',');
-            const item = {
-                from: values[0],
-                towhere: values[1],
-                date: values[2],
-                type: values[3]
-            };
-            items.push(item);
-        });
-        renderTable(items); // вызов функции renderTable
-    })
-    .catch(error => {
-        console.error(error);
+function getTableRow(item) {
+    const row = document.createElement('div');
+    const cells = ['from', 'towhere', 'date', 'type'].map(key => {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.textContent = item[key];
+        return cell;
     });
 
-function renderTable(items) {
-    table.innerHTML = ''; // очистка таблицы
-    for (const item of items) {
-        const row = getTableItem(item);
-        table.appendChild(row);
-    }
+    row.classList.add('row');
+    row.append(...cells);
+
+    return row;
 }
 
-
+// Функция отрисовки таблицы
+function renderTable(items) {
+    table.innerHTML = '';
+    items.forEach(item => table.appendChild(getTableRow(item)));
+}
 
 // Загрузка данных и отрисовка таблицы
 loadCSV()
@@ -82,14 +42,14 @@ loadCSV()
 
         // Поиск
         const form = document.getElementById('form');
+        const delay = 1000; // Задержка перед поиском
 
-        form.addEventListener('input', function () {
-            setTimeout(function () {
+        form.addEventListener('input', () => {
+            setTimeout(() => {
                 const from = document.getElementById('from').value.toLowerCase().trim();
                 const towhere = document.getElementById('towhere').value.toLowerCase().trim();
                 const type = document.getElementById('type').value.toLowerCase().trim();
-                const table = document.getElementById('table');
-
+                
                 const filteredItems = items.filter(item => {
                     const itemFrom = item.from.toLowerCase();
                     const itemTowhere = item.towhere.toLowerCase();
@@ -97,9 +57,8 @@ loadCSV()
                     return itemFrom.includes(from) && itemTowhere.includes(towhere) && itemType.includes(type);
                 });
 
-                table.innerHTML = ''; // очищаем таблицу перед добавлением новых элементов
-
                 renderTable(filteredItems);
-            }, 1000);
+            }, delay);
         });
-    });
+    })
+    .catch(error => console.error(error));
